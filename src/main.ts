3565706fa0626c4de7adf9d1e013f614acfdb34b
Petteri Stenius <petteri.stenius@gmail.com>
2023-02-11 13:03:58 +0200

    split test cases into sub modules

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,15 @@
 import './style.css'
-import libsodium from "libsodium-wrappers-sumo";
-import { Signer } from '../signify-ts/src/keri/core/signer';
-import { b } from "../signify-ts/src/keri/core/core";
-import { Base64 } from "../signify-ts/src/keri/util/helper";
-// import load from 'blake3/browser-async';
-// import * as blake3 from 'blake3/browser';
 
 document.getElementById("sign")!.addEventListener("click", async () => {
-  await libsodium.ready;
-
   const output = document.getElementById("output")!;
   output.innerText = "";
-
-  const message = b(document.getElementById("message")!.innerText);
-
-  const signer = new Signer({});
-  output.appendChild(document.createTextNode(`publicKey ${Base64.encode(signer.verfer.raw)}\r\n`));
-
-  const signature = signer.sign(message);
-  output.appendChild(document.createTextNode(`signature ${Base64.encode(signature.raw)}\r\n`));
-
-  const result = signer.verfer.verify(signature.raw, message)
-  console.assert(result, "signer.verfer.verify");
+  const signer = (await import("./signer")).signer;
+  await signer(output, document.getElementById("message")!.innerText);
 });
 
 document.getElementById("digest")!.addEventListener("click", async () => {
   const output = document.getElementById("output")!;
   output.innerText = "";
-  output.appendChild(document.createTextNode(`blake3 is not yet implemented\r\n`));
+  const digest = (await import("./digest")).digest;
+  await digest(output, document.getElementById("message")!.innerText);
 });
