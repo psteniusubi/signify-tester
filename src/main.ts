c907bacb59534a08cc17dd3c7ba3ab3ee5d0391b
Petteri Stenius <petteri.stenius@gmail.com>
2023-08-29 19:18:49 +0300

    updates

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ document.getElementById("boot")!.addEventListener("click", async () => {
   }
   document.getElementById("output")?.append(`controller=${client.controller.pre}\r\n`);
   document.getElementById("output")?.append(`agent=${client.agent?.pre}\r\n`);
-  let i = client.identifiers().list();
-  document.getElementById("output")?.append(`identifiers: ${JSON.stringify(i)}\r\n`);
+  let res = await client.identifiers().list();
+  document.getElementById("output")?.append(`identifiers: ${JSON.stringify(res)}\r\n`);
 });
