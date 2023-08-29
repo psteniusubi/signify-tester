a0785168b4e4ee72cbdccb6b3a38de66bcc9c1b7
Petteri Stenius <petteri.stenius@gmail.com>
2023-08-29 18:21:51 +0300

    updates to recent signify-ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import './style.css'
+import { ready, SignifyClient, Tier } from 'signify-ts/src';
 
-document.getElementById("sign")!.addEventListener("click", async () => {
-  const output = document.getElementById("output")!;
-  output.innerText = "";
-  const signer = (await import("./signer")).signer;
-  await signer(output, document.getElementById("message")!.innerText);
-});
-
-document.getElementById("digest")!.addEventListener("click", async () => {
-  const output = document.getElementById("output")!;
-  output.innerText = "";
-  const digest = (await import("./digest")).digest;
-  await digest(output, document.getElementById("message")!.innerText);
+document.getElementById("boot")!.addEventListener("click", async () => {
+  await ready();
+  let client = new SignifyClient("http://localhost:3901", "client1".padEnd(21, "_"), Tier.low);
+  try {
+    await client.connect();
+  } catch {
+    await client.boot();
+    await client.connect();
+  }
+  document.getElementById("output")?.append(`${client.controller.pre}\r\n`);
+  document.getElementById("output")?.append(`${client.agent?.pre}\r\n`);
 });
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,4 @@
-import * as path from 'path'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            "platform": path.resolve(__dirname, 'src/platform-browser.ts'),
-        }
-    }
 });
