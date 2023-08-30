e2a9b2330495e35d7c7d146b2fecd0dce780d3b3
Petteri Stenius <petteri.stenius@gmail.com>
2023-08-30 09:38:45 +0300

    adding features

--- src/signify.ts
+++ src/signify.ts
@@ -1,21 +1,12 @@
-import { ready, SignifyClient, Tier } from 'signify-ts/src';
-
-export async function connect(bran: string, hostname: string): Promise<SignifyClient> {
-    await ready();
-    let client = new SignifyClient(`http://${hostname}:3901`, bran.padEnd(21, "_"), Tier.low, `http://${hostname}:3903`);
-    try {
-        await client.connect();
-    } catch {
-        await client.boot();
-        await client.connect();
-    }
-    return client;
-}
+import { SignifyClient } from 'signify-ts/src';
+import { sleep } from './helper';
 
 export async function wait_operation(client: SignifyClient, op: any): Promise<any> {
+    let ms = 500;
     while (!op.done) {
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise(resolve => setTimeout(resolve, ms));
         op = await client.operations().get(op.name);
+        ms *= 1.2;
     }
     return op;
 }
