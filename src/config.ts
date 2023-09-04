3d7380e76eb2a619344dfb74db92df16cfc92933
Petteri Stenius <petteri.stenius@gmail.com>
2023-09-04 12:36:06 +0300

    implement passcode history

--- src/config.ts
+++ src/config.ts
@@ -35,3 +35,33 @@ switch (url.hostname) {
 export const WITS = _WITS;
 export const KERIA_ADMIN = _KERIA_ADMIN;
 export const KERIA_BOOT = _KERIA_BOOT;
+
+const KEY: string = "signify-tester/passcode";
+
+export async function get_passcodes(): Promise<Array<string>> {
+    let value = localStorage.getItem(KEY);
+    if (value === null) return [];
+    try {
+        let result = JSON.parse(value);
+        if (result instanceof Array) {
+            return Array.from(new Set(result));
+        }
+    } catch {
+        return [value];
+    }
+    return [];
+}
+
+export async function save_passcode(value: string): Promise<void> {
+    let history = new Set(await get_passcodes());
+    history.add(value);
+    let list = Array.from(history).sort();
+    localStorage.setItem(KEY, JSON.stringify(list));
+}
+
+export async function remove_passcode(value: string): Promise<void> {
+    let history = new Set(await get_passcodes());
+    history.delete(value);
+    let list = Array.from(history).sort();
+    localStorage.setItem(KEY, JSON.stringify(list));
+}
