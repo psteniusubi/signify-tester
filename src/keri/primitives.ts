339da34151cc64ab233b4d1f7bbbe04cf39f1113
Petteri Stenius <petteri.stenius@gmail.com>
2023-10-30 18:08:23 +0200

    updates

--- src/keri/primitives.ts
+++ src/keri/primitives.ts
@@ -1,3 +1,10 @@
+/*
+
+https://softwareengineering.stackexchange.com/questions/437593/is-it-ok-to-have-type-aliases-for-primitive-types-in-typescript 
+https://stackoverflow.com/questions/55781559/what-does-the-as-keyword-do
+ 
+*/
+
 declare abstract class As<Tag extends keyof never> {
     private static readonly $as$: unique symbol;
     private [As.$as$]: Record<Tag, true>;
@@ -6,7 +13,7 @@ declare abstract class As<Tag extends keyof never> {
 export type QB64 = string & As<"QB64">;
 export type AID = string & QB64 & As<"AID">;
 
-export function isQB64(value: string): value is QB64 {
+export function isQB64(value: string): value is QB64 | AID {
     // TODO: check value is base64url
     return true;
 }
